ddaf0ddf6c71bdf2066f3a7f3018a6dad4003452
Danylo Pidhorny <dquark74@protonmail.com>
2026-01-11 19:42:33 +0200

    feat: db migrations

---
+++ backend/prisma/migrations/20260111174215_improve_notifications/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "notifications" DROP CONSTRAINT "notifications_user_id_fkey";
+
+-- CreateIndex
+CREATE INDEX "notifications_user_id_idx" ON "notifications"("user_id");
+
+-- CreateIndex
+CREATE INDEX "notifications_user_id_read_idx" ON "notifications"("user_id", "read");
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
