34bf4ee8aa99622720babf12e0f8bdeb78c34ddd
Danylo Pidhorny <dquark74@protonmail.com>
2026-01-17 15:08:09 +0200

    feat: enable supabase

--- backend/src/modules/profile/profile.controller.ts
+++ backend/src/modules/profile/profile.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ClerkUserId } from 'src/decorators/clerk.decorator';
 import { PROFILE_ENUMS } from 'src/enums/routes.enums';
-import { ClerkAuthGuard } from 'src/guards/clerk.guard';
 import { ProfileService } from './profile.service';
 import { User } from 'src/types/user/user.types';
 
-@UseGuards(ClerkAuthGuard)
 @Controller(PROFILE_ENUMS.BASE)
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
